86e9bb93515b54bf84d05d36060db4ff293dd7ca
droperZero <lucas_front@yahoo.com.br>
2018-06-04 00:52:13 -0300

    inicio da avaliacao de java 1

---
+++ JAVA/avaliacao_java_persistencia/src/br/com/fiap/entity/Curso.java
@@ -0,0 +1,20 @@
+package br.com.fiap.entity;
+
+public class Curso {
+	private int id;
+	private String nome;
+	private int id_escola;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
