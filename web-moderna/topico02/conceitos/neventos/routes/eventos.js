4c31ba466d9959db9f64b6193da5cc078905ed38
Lucas Dias Lopes <lucas_front@yahoo.com.br>
2018-06-13 22:37:17 -0300

    Aula de nodejs

---
+++ web-moderna/topico02/conceitos/neventos/routes/eventos.js
@@ -0,0 +1,10 @@
+module.exports = function(app) {
+  var eventos = app.controllers.eventos;
+  var valida = require("./../middlewares/valida");
+
+  app.get('/menu', valida, eventos.menu);
+
+  app.get('/cadUsuario', valida, eventos.cadastroUsuario);
+  app.get('/cadEvento', valida, eventos.cadastroEvento);
+  app.get('/listaEventos', valida, eventos.listaEventos);
+};
