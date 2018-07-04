952d067eb117275b988b6d08db3ce714826f2d08
magnorion <lucas_front@yahoo.com.br>
2018-07-04 10:58:44 -0300

    add check de array vazio

--- JAVA/avaliacao_java_persistencia/src/br/com/fiap/app/MainApp.java
+++ JAVA/avaliacao_java_persistencia/src/br/com/fiap/app/MainApp.java
@@ -101,12 +101,19 @@ public class MainApp {
 	private static void listarCursos(Escola escola) {
 		try {
 			List<EscolaCursoViewModel> cursos = processoListagemCurso(escola);
-			Icon icon = UIManager.getIcon("OptionPane.alertIcon");
-			EscolaCursoViewModel opcao = (EscolaCursoViewModel) JOptionPane.showInputDialog(null,  
-	                "Escolha uma opção", "Opções",  
-	                JOptionPane.PLAIN_MESSAGE, icon, cursos.toArray(), "Opções");
 			
-			viewEscola(escola);
+			if (cursos.isEmpty()) {
+				JOptionPane.showMessageDialog(null, "Não existem cursos cadastrados!\nPor favor, cadastre um curso!");
+				viewEscola(escola);
+			} else {
+				Icon icon = UIManager.getIcon("OptionPane.alertIcon");
+				EscolaCursoViewModel opcao = (EscolaCursoViewModel) JOptionPane.showInputDialog(null,  
+		                "Escolha uma opção", "Opções",  
+		                JOptionPane.PLAIN_MESSAGE, icon, cursos.toArray(), "Opções");
+				
+				viewEscola(escola);
+			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -152,12 +159,17 @@ public class MainApp {
 	private static void listarEscolas() {
 		EscolaHelper helper = new EscolaHelper(setEm());
 		List<Escola> escolas = helper.listarEscolas();
+		
+		if (escolas.isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Não existe escolas cadastradas!\nPor favor, cadastre uma escola!");
+			rotas("inicio", null);
+		} else {
+			Icon icon = UIManager.getIcon("OptionPane.alertIcon");
+			Escola opcao = (Escola) JOptionPane.showInputDialog(null, "Escolha uma opção", "Opções",
+					JOptionPane.PLAIN_MESSAGE, icon, escolas.toArray(), "Opções");
 
-		Icon icon = UIManager.getIcon("OptionPane.alertIcon");
-		Escola opcao = (Escola) JOptionPane.showInputDialog(null, "Escolha uma opção", "Opções",
-				JOptionPane.PLAIN_MESSAGE, icon, escolas.toArray(), "Opções");
-
-		viewEscola(opcao);
+			viewEscola(opcao);
+		}
 	}
 
 	/**
@@ -209,26 +221,40 @@ public class MainApp {
 		AlunoHelper helper = new AlunoHelper(setEm());
 		
 		List<EscolaCursoViewModel> cursos = processoListagemCurso(aluno.getEscola());
-		Icon icon = UIManager.getIcon("OptionPane.alertIcon");
-		EscolaCursoViewModel opcao = (EscolaCursoViewModel) JOptionPane.showInputDialog(null,  
-                "Escolha um curso para o cadastro", "Opções",  
-                JOptionPane.PLAIN_MESSAGE, icon, cursos.toArray(), "Opções");
 		
-		JOptionPane.showMessageDialog(null, helper.cadastraCurso(aluno, opcao.getCursoId()));
-		viewAluno(aluno);
+		if (cursos.isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Não existem cursos cadastrados!\nPor favor, cadastre um curso!");
+			viewAluno(aluno);
+		} else {
+			Icon icon = UIManager.getIcon("OptionPane.alertIcon");
+			EscolaCursoViewModel opcao = (EscolaCursoViewModel) JOptionPane.showInputDialog(null,  
+	                "Escolha um curso para o cadastro", "Opções",  
+	                JOptionPane.PLAIN_MESSAGE, icon, cursos.toArray(), "Opções");
+			
+			JOptionPane.showMessageDialog(null, helper.cadastraCurso(aluno, opcao.getCursoId()));
+			viewAluno(aluno);
+		}
 	}
 	
 	private static void listaAlunos(Escola escola) {
 		try {
 			List<CursoAlunoViewModel> alunos = processoListagemAluno(escola);
-			Icon icon = UIManager.getIcon("OptionPane.alertIcon");
-			CursoAlunoViewModel opcao = (CursoAlunoViewModel) JOptionPane.showInputDialog(null,  
-	                "Escolha uma aluno para dar uma nota.", "Alunos",  
-	                JOptionPane.PLAIN_MESSAGE, icon, alunos.toArray(), "Opções");
 			
-			Aluno aluno = pegarAluno(opcao.getId());
+			if (alunos.isEmpty()) {
+				JOptionPane.showMessageDialog(null, "Não existem alunos cadastrados!\nPor favor, cadastre um aluno!");
+				viewEscola(escola);
+			} else {
+				Icon icon = UIManager.getIcon("OptionPane.alertIcon");
+				CursoAlunoViewModel opcao = (CursoAlunoViewModel) JOptionPane.showInputDialog(null,  
+		                "Escolha uma aluno para dar uma nota.", "Alunos",  
+		                JOptionPane.PLAIN_MESSAGE, icon, alunos.toArray(), "Opções");
+				
+				Aluno aluno = pegarAluno(opcao.getId());
+				
+				viewAluno(aluno);
+			}
 			
-			viewAluno(aluno);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -254,55 +280,67 @@ public class MainApp {
 	
 	public static void aplicarNota(Aluno aluno, Escola escola) {
 		Set<Curso> cursos = aluno.getCursos();
-		Icon icon = UIManager.getIcon("OptionPane.alertIcon");
-		Curso curso = (Curso) JOptionPane.showInputDialog(null,  
-                "Escolha um curso", "Opções",  
-                JOptionPane.PLAIN_MESSAGE, icon, cursos.toArray(), "Opções");
 		
-		Boolean flag = true;
-		String nota;
-		Float notaFinal = new Float(0);
-		
-		while (flag) {
-			nota = JOptionPane.showInputDialog("Digite a nota do aluno");
-			notaFinal = Float.parseFloat(nota);
-			
-			if (notaFinal > 10 || notaFinal < 0) {
-				JOptionPane.showMessageDialog(null, "Nota incorreta!\nA nota deve ser de 0 a 10");
-			} else {
-				flag = false;
-			}
-		}
-		
-		try {
-			AlunosCursosHelper helper = new AlunosCursosHelper(setEm());
-			JOptionPane.showMessageDialog(null, helper.nota(aluno.getId(), curso.getId(), notaFinal));
-			
+		if (cursos.isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Este aluno não esta cadastrado em um curso!");
 			viewAluno(aluno);
-		} catch (Exception e) {
-			e.printStackTrace();
+		} else { 
+			Icon icon = UIManager.getIcon("OptionPane.alertIcon");
+			Curso curso = (Curso) JOptionPane.showInputDialog(null,  
+	                "Escolha um curso", "Opções",  
+	                JOptionPane.PLAIN_MESSAGE, icon, cursos.toArray(), "Opções");
+			
+			Boolean flag = true;
+			String nota;
+			Float notaFinal = new Float(0);
+			
+			while (flag) {
+				nota = JOptionPane.showInputDialog("Digite a nota do aluno");
+				notaFinal = Float.parseFloat(nota);
+				
+				if (notaFinal > 10 || notaFinal < 0) {
+					JOptionPane.showMessageDialog(null, "Nota incorreta!\nA nota deve ser de 0 a 10");
+				} else {
+					flag = false;
+				}
+			}
+			
+			try {
+				AlunosCursosHelper helper = new AlunosCursosHelper(setEm());
+				JOptionPane.showMessageDialog(null, helper.nota(aluno.getId(), curso.getId(), notaFinal));
+				
+				viewAluno(aluno);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
 	public static void cursosDoAluno(Aluno aluno) {
 		Set<Curso> cursos = aluno.getCursos();
-		Icon icon = UIManager.getIcon("OptionPane.alertIcon");
-		Curso curso = (Curso) JOptionPane.showInputDialog(null,  
-                "Escolha um curso", "Opções",  
-                JOptionPane.PLAIN_MESSAGE, icon, cursos.toArray(), "Opções");
-	
-		try {
-			AlunosCursosHelper helper = new AlunosCursosHelper(setEm());
-			Float nota = helper.mostraCursoNota(aluno, curso);
-			if (nota > 0) {
-				JOptionPane.showMessageDialog(null, "Neste curso, a nota do aluno é " + nota);
-			} else {
-				JOptionPane.showMessageDialog(null, "Neste curso, o aluno foi reporvado ou ainda não teve nota!");
-			}
-			
+		
+		if (cursos.isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Este aluno não esta cadastrado em um curso!");
 			viewAluno(aluno);
-		} catch (Exception e) {
-			e.printStackTrace();
+		} else {
+			Icon icon = UIManager.getIcon("OptionPane.alertIcon");
+			Curso curso = (Curso) JOptionPane.showInputDialog(null,  
+	                "Escolha um curso", "Opções",  
+	                JOptionPane.PLAIN_MESSAGE, icon, cursos.toArray(), "Opções");
+		
+			try {
+				AlunosCursosHelper helper = new AlunosCursosHelper(setEm());
+				Float nota = helper.mostraCursoNota(aluno, curso);
+				if (nota > 0) {
+					JOptionPane.showMessageDialog(null, "Neste curso, a nota do aluno é " + nota);
+				} else {
+					JOptionPane.showMessageDialog(null, "Neste curso, o aluno foi reporvado ou ainda não teve nota!");
+				}
+				
+				viewAluno(aluno);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
